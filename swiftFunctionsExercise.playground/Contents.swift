0c77b9eabf5895b270bfbb44e947d488b4d77c6a
Bananaslama277 <tracesmith555@gmail.com>
2023-01-26 08:42:01 -0600

    Main Complete

---
+++ swiftFunctionsExercise.playground/Contents.swift
@@ -0,0 +1,19 @@
+func phineasFerb() {
+    print("Hey Ferb, I know what we're gonna do today!")
+}
+phineasFerb()
+
+func agentP(perry: String) {
+    print(perry + "Doo-Bee-Doo-Bee-Do-Bah")
+}
+agentP(perry: "Doo-Bee-")
+
+func doof(name: String, animal: String) {
+    print("Curse you, " + name + " the " + animal + "!")
+}
+doof(name: "Perry", animal: "Platypus")
+
+func isabella() -> String {
+    return "What'cha Doin?"
+}
+isabella()
